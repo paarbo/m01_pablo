b71e4fe8709dc0c0cdba260b9bb54fb00c83521e
ism23843564 <ism23843564@a12.informatica.escoladeltreball.org>
2018-04-16 09:40:09 +0200

    0.1

---
+++ test.py
@@ -0,0 +1,8 @@
+
+num1=int(input("Introduce un numero: "))
+num2=int(input("Introduce otro mayor al anterior: "))
+num=0
+while num1>num2:
+    num2=int(input("Pon otro: "))
+print("Los numeros escritos son",num2,"y",num1)
+num+=1
